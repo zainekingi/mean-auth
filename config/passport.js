3a02b07a3630189fc619490050404cabe5c65787
zaine-kingi <zainekingi@users.noreply.github.com>
2017-06-02 20:17:37 +1200

    remove unknown code from merge from remote repo.

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,6 @@
  * FILE: PASSPORT.JS
  * AUTHOR: Zaine Kingi
  * VER: 1.0
-<<<<<<< HEAD
  * DESC: Passport strategy application file.
  * -------------------------------------------------- */
 
@@ -35,33 +34,3 @@ module.exports = (passport) => {
     });
   }));
 }
-=======
- * DESC: Main configuration file for the application
- *       passport-jwt strategy.
- * -------------------------------------------------- */
-
- // file dependencies.
- const JwtStrategy = require('passport-jwt').Strategy;
- const ExtractJwt = require('passport-jwt').ExtractJwt;
- const User = require('../models/users');
- const config = require('../config/database');
-
- // export the strategy function.
- module.exports = (passport) => {
-     let opts = {};
-     opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
-     opts.secretOrKey = config.secret;
-     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-         User.getUserById(jwt_payload._doc._id, (err, user) => {
-             if(err) {
-                 return done(err, false);
-             }
-             if(user) {
-                 return done(null, user);
-             } else {
-                 return done(null, false);
-             }
-         });
-    }));
-}
->>>>>>> 143b5d7f72932e680f12512bab10e958279f9b7e

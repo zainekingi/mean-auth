303dcf8a263f0655a670292b948612e64a50794e
zaine-kingi <zainekingi@users.noreply.github.com>
2017-06-02 19:57:12 +1200

    add authentication route method passing user entered data and comparing with users records, protect profile route and send user information on success.

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const router = express.Router();
 const User = require('../models/users');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 // =================================================
 // START: User routes.
@@ -43,13 +44,64 @@ router.post('/register', (req, res, next) => {
 });
 
 // authenticate.
-router.post('/auth', (req, res, next) => {
-    res.send('Users Auth');
+router.post('/authenticate', (req, res, next) => {
+  // Get submitted data to validate.
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // See if a username exists.
+  User.getUserByUsername(username, (err, user) => {
+    // Check for errors.
+    if(err) throw err;
+
+    // Check if username exists.
+    if(!user) {
+      return res.json({
+        success: false,
+        message: 'Ooh! user not found!'
+      });
+    }
+
+    // User record returned - compare passwords.
+    User.comparePassword(password, user.password, (err, isMatch) => {
+
+      // Check for errors.
+      if(err) throw err;
+
+      // Check password with stored password.
+      if(isMatch) {
+
+        // Create the token.
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // Set the expiry time - 1 week in secs.
+        });
+
+        // Send responds to the frontend.
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            name: user.username,
+            email: user.email
+          }
+
+        });
+      } else {
+        // Wrong password.
+        res.json({
+          success: false,
+          message: 'Wrong password'
+        });
+      }
+    });
+  });
 });
 
 // profile.
-router.get('/profile', (req, res, next) => {
-    res.send('User Profile');
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+  // Send the user data.
+  res.json({user: req.user});
 });
 
 // =================================================

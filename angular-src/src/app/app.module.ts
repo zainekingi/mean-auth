fbb78a106bedfed7e09282e52658ea56d69cc5a2
zaine-kingi <zainekingi@users.noreply.github.com>
2017-06-03 11:04:38 +1200

    first initial commit.

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+
+// App routes config array.
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },               // Home route.
+  { path: 'login', component: LoginComponent },        // Login route.
+  { path: 'register', component: RegisterComponent },  // Register route.
+  { path: 'dashboard', component: DashboardComponent },// Dashboard route.
+  { path: 'profile', component: ProfileComponent }     // Profile route.
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ProfileComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

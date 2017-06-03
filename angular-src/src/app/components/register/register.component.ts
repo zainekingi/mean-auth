94cdb9e3750e9dd82b17a046acd2cf0cf3b44094
zaine-kingi <zainekingi@users.noreply.github.com>
2017-06-03 19:18:05 +1200

    add ValidateService, FlashMessagesService imports, constructor definitions and onRegister registration form validattion function with flash-messaging.

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(private validateService: ValidateService, private flashMessages: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegister() {
+    // create new user object.
+    const newUser = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+
+    // Required fields.
+    if(!this.validateService.validateRegister(newUser)) {
+      // display error.
+      this.flashMessages.show('Please fill in all fields!', { cssClass: 'alert-danger', timeout: 5000 });
+
+      // return false.
+      return false;
+    }
+
+    // Validate email.
+    if(!this.validateService.validateEmail(newUser.email)) {
+      // display error.
+      this.flashMessages.show('Please use a valid email!', { cssClass: 'alert-danger', timeout: 5000 });
+
+      // return false.
+      return false;
+    }
   }
 
 }

cc27d885ade16b9c274a8f709f865e2ca5e80a64
zaine-kingi <zainekingi@users.noreply.github.com>
2017-06-10 21:44:39 +1200

    add authenticateUser and storeUserData functions.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -20,4 +20,27 @@ export class AuthService {
       .map(res => res.json());
   }
 
+  authenticateUser(user) {
+
+    // Set the content-type of the headers.
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    // return the observable.
+    return this.http.post('http://localhost:3000/users/authenticate', user, { headers:headers })
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+
+    // Store in local storage.
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+
+    // Set the component
+    this.authToken = token;
+    this.user = user;
+
+  }
+
 }

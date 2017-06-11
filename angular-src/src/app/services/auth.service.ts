90509da60905b8eb5612c52d2526710090c020e9
zainesS9 <rous6026nz@gmail.com>
2017-06-11 20:45:38 +1200

    add logOut function.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -9,6 +9,7 @@ export class AuthService {
 
   constructor(private http: Http) { }
 
+  // Register user.
   registerUser(user) {
 
     // Set the content-type of the headers.
@@ -18,6 +19,37 @@ export class AuthService {
     // return the observable.
     return this.http.post('http://localhost:3000/users/register', user, { headers:headers })
       .map(res => res.json());
+  }
+
+  // Log user in.
+  authenticateUser(user) {
+
+    // Set the content-type of the headers.
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    // Return the observable.
+    return this.http.post('http://localhost:3000/users/authenticate', user, { headers:headers })
+      .map(res => res.json());
+  }
+
+  // Store user data in local storage.
+  storeUserData(token, user) {
+    // set items in local storage.
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+
+    // set component properties.
+    this.authToken = token;
+    this.user = user;
+  }
+
+  // Logout user.
+  logOut() {
+    // clear local storage.
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 
 }

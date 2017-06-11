05e077bad1a5d5bd40e469de89aebddfe469d40f
zainesS9 <rous6026nz@gmail.com>
2017-06-11 20:41:17 +1200

    add onLogout function, file depenancies and inject dependancies into the component constructor.

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private flashMessages: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogout() {
+    // clear local storage.
+    this.authService.logOut();
+
+    // display success message.
+    this.flashMessages.show('You are logged out!', { cssClass: 'alert-success', timeout: 3000 });
+
+    // redirect user to login page.
+    this.router.navigate(['/login']);
   }
 
 }

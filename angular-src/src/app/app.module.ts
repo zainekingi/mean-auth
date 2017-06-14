d73780d0f0089fe409f5b8f7788548f54fcd00f7
zaine-kingi <zainekingi@users.noreply.github.com>
2017-06-14 21:06:19 +1200

    import AuthGuard and add as a provider.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -14,14 +14,15 @@ import { HomeComponent } from './components/home/home.component';
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
 
 // App routes config array.
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },               // Home route.
-  { path: 'login', component: LoginComponent },        // Login route.
-  { path: 'register', component: RegisterComponent },  // Register route.
-  { path: 'dashboard', component: DashboardComponent },// Dashboard route.
-  { path: 'profile', component: ProfileComponent }     // Profile route.
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
@@ -41,7 +42,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
